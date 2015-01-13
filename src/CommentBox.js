7877aa529f2e637a7a3d8822cdd9916ee9251ca3
Jordi Anguela <jordi.anguela@gmail.com>
2015-01-13 15:25:43 +0100

    Add CommentList and CommentForm subcomponents to CommentBox

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,11 +1,35 @@
 var React = require('react');
 
 var CommentBox = React.createClass({
-	
-	render: function() {
-		return <div>Comment Box</div>;
-	}
-	
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h2>Comments</h2>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
 });
 
 module.exports = CommentBox;
