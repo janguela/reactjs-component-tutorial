585894f0a4d630e03d0630b2a47f36ba4ea925ec
Jordi Anguela <jordi.anguela@gmail.com>
2015-01-13 16:44:40 +0100

    Add text Markdown

--- src/Comment.js
+++ src/Comment.js
@@ -1,13 +1,15 @@
 var React = require('react');
 
+var converter = new Showdown.converter();
 var Comment = React.createClass({
 	render: function() {
+		var rawMarkup = converter.makeHtml(this.props.children.toString());
 		return (
 			<div className="comment">
 				<h3 className="commentAuthor">
 					{this.props.author}
 				</h3>
-				{this.props.children}
+				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
 			</div>
 		);
 	}
